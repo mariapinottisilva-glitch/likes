2b4d53687d85af702386239989a80dd721810043
mariapinottisilva-glitch <maria.pinotti.silva@escola.pr.gov.br>
2026-08-26 07:51:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const botoes = document.querySelectorAll(".like");
 
+const contador = document.querySelector("#contador");
+
+botoes.forEach(function(botaoClicado){
+  botaoClicado("click", funvtion(){
+      const valorSelecionado = Nunber(botaoclicado.dataset.valor);
+      contador.textContent = valorSelecionado;
+      botoes.forEach(function(botaoAtual){
+        const valorAtual =Nunber(botaoAtual.dataset.valor);
+        if (valorAtual <= valorSelecionado){
+           botaoAtual.classit.add("ativo")
+        } else {
+           botaoAtual.classssit.remove("ativo");
+        }
+      });
+  });
+});
